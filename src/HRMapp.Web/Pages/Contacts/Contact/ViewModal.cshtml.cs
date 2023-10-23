5ced742a0b9bc5a82fa53fe5252ba4bda8e18fc2
nowaynofr <niemvuilaso1@gmail.com>
2023-10-23 21:46:52 +0700

    binh dich

---
+++ src/HRMapp.Web/Pages/Contacts/Contact/ViewModal.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using HRMapp.Contacts;
+using HRMapp.Contacts.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using HRMapp.Employees;
+using HRMapp.Employees.Dtos;
+using HRMapp.Web.Pages.Employees.Employee.ViewModels;
+
+namespace HRMapp.Web.Pages.Contacts.Contact;
+
+public class ViewModalModel : HRMappPageModel
+{
+    [HiddenInput]
+    [BindProperty(SupportsGet = true)]
+    public Guid Id { get; set; }
+    
+    [BindProperty]
+    public ContactDto ViewModel { get; set; }
+
+    private readonly IContactAppService _service;
+
+    public ViewModalModel(IContactAppService service)
+    {
+        _service = service;
+    }
+
+    public virtual async Task OnGetAsync()
+    {
+        var dto = await _service.GetAsync(Id);
+        ViewModel = dto;
+    }
+}
---
+++ src/HRMapp.Web/Pages/Contacts/CreateOrEditModal.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+using HRMapp.Contacts;
+using HRMapp.Contacts.Dtos;
+using HRMapp.Web.Pages.Contacts.Contact.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRMapp.Web.Pages.Contacts;
+
+public class CreateOrEditModalModel : HRMappPageModel
+{
+    [HiddenInput]
+    [BindProperty(SupportsGet = true)]
+    public Guid Id { get; set; }
+    [BindProperty]
+    public CreateEditContactViewModel ViewModel { get; set; }
+
+    private readonly IContactAppService _service;
+
+    public CreateOrEditModalModel(IContactAppService service)
+    {
+        _service = service;
+    }
+
+    public virtual async Task OnGetAsync()
+    {
+        if (!IsCreate())
+        {
+            var dto = await _service.GetAsync(Id);
+            
+            ViewModel = ObjectMapper.Map<ContactDto, CreateEditContactViewModel>(dto);
+        }
+        else
+        {
+            ViewModel = new CreateEditContactViewModel();
+        }
+    }
+
+    public virtual async Task<IActionResult> OnPostAsync()
+    {
+        if (!IsCreate())
+        {
+            var dto = ObjectMapper.Map<CreateEditContactViewModel, CreateUpdateContactDto>(ViewModel);
+            await _service.UpdateAsync(Id, dto);
+        }
+        else
+        {
+            var dto = ObjectMapper.Map<CreateEditContactViewModel, CreateUpdateContactDto>(ViewModel);
+            await _service.CreateAsync(dto);
+        }
+        return NoContent();
+    }
+    
+    bool IsCreate()
+    {
+        if (Id != Guid.Empty)
+        {
+            return false;
+        }
+        return true;
+    }
+}
